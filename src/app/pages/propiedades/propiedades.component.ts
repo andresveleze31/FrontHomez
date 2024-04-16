abd68117231b14cf886bef017758f2b638d40315
andresveleze31 <142637448+andresveleze31@users.noreply.github.com>
2024-04-16 06:56:45 -0500

    Propiedades, Blog, Best Secciones

--- src/app/pages/propiedades/propiedades.component.ts
+++ src/app/pages/propiedades/propiedades.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { PropiedadCardComponent } from '../../components/propiedad-card/propiedad-card.component';
+import { Propiedad } from '../../models/Propiedad';
+import { PropiedadService } from '../../services/propiedad/propiedad.service';
 
 @Component({
   selector: 'app-propiedades',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    CommonModule,
+    PropiedadCardComponent,
+  ],
   templateUrl: './propiedades.component.html',
   styleUrl: './propiedades.component.scss',
 })
-export class PropiedadesComponent {}
+export class PropiedadesComponent {
+  propiedades: Propiedad[] = [];
+
+  constructor(private propiedadService: PropiedadService) {}
+
+  ngOnInit(): void {
+    this.cargarPropiedades();
+  }
+
+  cargarPropiedades() {
+    this.propiedadService
+      .getPropiedades()
+      .then((post) => {
+        this.propiedades = post;
+        console.log(this.propiedades);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
