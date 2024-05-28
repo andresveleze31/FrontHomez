b79a8d06d0d85056e3a808175f5483ca39e033cc
andresveleze31 <142637448+andresveleze31@users.noreply.github.com>
2024-05-28 12:33:30 -0500

    Chat

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -31,7 +31,7 @@ export class ChatService {
   }
 
   initConnectionSocket() {
-    const url = '//https://gruposjaveriana.dynaco.co/api/homez/chat-socket';
+    const url = '//gruposjaveriana.dynaco.co/api/homez/chat-socket';
     const socket = new SockJS(url);
     this.stompClient = Stomp.over(socket);
   }
